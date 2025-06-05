42d84bb7b7b146a6c06fa8bf190a934aaa4c2106
Umesh bist <137031900+umesh-bist@users.noreply.github.com>
2025-06-05 16:05:44 +0545

    Update axiosInstance.js

--- frontend/frontend/src/reduxSaga/axiosInstance.js
+++ frontend/frontend/src/reduxSaga/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API=axios.create({
-    baseURL:"http://localhost:3000/api/auth/notes"
+    baseURL:"https://note-taking-app-mern-axi4.onrender.com/api/auth/notes"
 })
 
 API.interceptors.request.use((config) => {
